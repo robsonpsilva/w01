ea13dfb60b2f5828aa72f7ca37d158c62d4226fc
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-02-25 09:18:30 -0300

    modified:   controllers/index.js 	modified:   routes/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const getName = (req, res) => {
-  res.send('Sarah Birch');
+  res.send('Robson Paulo da Silva');
 };
 
 module.exports = { getName };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
+const app = express();
+
 const routes = require('express').Router();
 
 const baseController = require('../controllers');
+app.use
 
 routes.get('/', baseController.getName);
+
+routes.get('/profile', (req, res) => {
+    res.send('Hello World, This is profile router');
+  });
+   
+routes.get('/login', (req, res) => {
+res.send('Hello World, This is login router');
+});
+
+routes.get('/logout', (req, res) => {
+res.send('Hello World, This is logout router');
+});
 
 module.exports = routes;
